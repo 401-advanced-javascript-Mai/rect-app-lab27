bde2b6b4a0d458383f9f9e62273cd1f55326a1cd
maialshagarin <runner@fv-az32.ok1umoa0vq5e5bifrvdscfwqub.cx.internal.cloudapp.net>
2020-02-26 11:26:34 +0000

    Updates

---
+++ precache-manifest.ad3e1d18f949baf9f2192d55f6533340.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "232c3492df7e397164d20eb29ca30fb4",
+    "url": "/rect-app-lab27/index.html"
+  },
+  {
+    "revision": "1b2bb57949f517f27ed9",
+    "url": "/rect-app-lab27/static/css/main.4ade8586.chunk.css"
+  },
+  {
+    "revision": "1ae82d8cc1e4543a917e",
+    "url": "/rect-app-lab27/static/js/2.aa1d9bca.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/rect-app-lab27/static/js/2.aa1d9bca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b2bb57949f517f27ed9",
+    "url": "/rect-app-lab27/static/js/main.4dbffe03.chunk.js"
+  },
+  {
+    "revision": "3f634d24d6d92f81ce49",
+    "url": "/rect-app-lab27/static/js/runtime-main.905cf0c0.js"
+  }
+]);
